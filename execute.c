d44f144e7a00b5a56060f42918e85bb7cc3c87ea
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-12-05 11:24:04 -0500

    corrected segfault on whitespace only cmd

--- execute.c
+++ execute.c
@@ -16,7 +16,7 @@ void exec_cmds(char *cmd){
   char **cmds = parseargs(cmd,";\n");
   int i = 0;
   while(cmds[i]){
-    exec_cmd(cmds[i++]); // inside this, args are malloc'd and freed; no memory leal
+    if(cmds[i][0]) exec_cmd(cmds[i++]); // inside this, args are malloc'd and freed; no memory leak
   }
   // this is NOT REACHED with exit(); there's a memory leak in that case!!
   free(cmds);
@@ -24,6 +24,7 @@ void exec_cmds(char *cmd){
 
 void exec_cmd(char *cmd){
   char **args = parseargs(cmd," ");
+  if(!args) return;
   if(!strcmp(args[0],"cd")){
     if( chdir(args[1]) < 0 ){
       printerr();
