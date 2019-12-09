3ae44927cd80fe9f2ec1f4171186f73c8be42100
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-12-09 18:10:39 -0500

    headers for the helper functions

--- redirect.h
+++ redirect.h
@@ -1,6 +1,10 @@
 #ifndef DEFINE_REDIRECT_H
 #define DEFINE_REDIRECT_H
 
+int redirect(int fd1,int fd2);
+
+int redirect_filename(char **args,int i,int fd_std,char *filename,int flags);
+
 void remove_blanks(char **args);
 
 int parse_redirects(int *bk_pointer, char **args);
